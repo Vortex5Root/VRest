9c982e84e4184f81e78f28bfbdb403c967f8036b
vortex <vortex@debian.lan>
2023-09-21 11:04:41 +0100

    WebSocket Added

--- README.md
+++ README.md
@@ -1,77 +1,124 @@
-# Welcome to VRest
+# VRest Documentation
 
-VRest is a powerful Python library designed for simplifying the process of making RESTful API calls. It provides an intuitive and user-friendly interface, enabling developers to effortlessly create and execute API requests.
+## Introduction
+
+VRest is a Python library for making HTTP and WebSocket requests to RESTful and WebSocket-based APIs. It provides a flexible and easy-to-use interface for interacting with various APIs, allowing you to send requests and receive responses.
 
 ## Installation
 
-To get started with VRest, you can easily install it using Poetry, a dependency management tool. Follow the instructions below to add VRest to your project:
+You can install VRest using pip:
 
 ```bash
 poetry add git+https://github.com/Vortex5Root/VRest.git
 ```
 
-By running this command, Poetry will fetch the latest version of VRest from the specified Git repository and add it as a dependency to your project.
+## Quick Start
+
+To get started with VRest, you need to create a `RestAPI` instance and define your API configuration, including the API endpoint, headers, and available functions. Here's a basic example using the Deepgram API:
+
+```python
+from VRest.vrest.new_version import RestAPI
+
+# Define the API configuration
+deepgram_config = {
+    "end_point": "https://api.deepgram.com/v1/",
+    "header": {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "*Authorization": "Token {}"
+    },
+    "skeloton": {
+        "pre_recode": {
+            "suffix": "listen",
+            "method": "POST"
+        },
+        "stream": {
+            "suffix": "listen",
+            "method": "ws"
+        }
+    }
+}
+
+# Create a RestAPI instance
+deepgram_api = RestAPI(deepgram_config, api_key="YOUR_API_KEY")
+
+# Example API request
+response = deepgram_api.pre_recode(params={"filler_words": "false", "summarize": "v2"}, json={"url": "https://static.deepgram.com/examples/interview_speech-analytics.wav"})
+
+# Print the response status code and data
+print(f"Status Code: {response.status_code}")
+if response.status_code == 200:
+    print(response.json()["results"]["channels"][0]["alternatives"][0]["transcript"])
+else:
+    print(response.text)
+```
 
 ## Usage
 
-### Creating a Dictionary
+### Creating a RestAPI Instance
 
-VRest requires a configuration dictionary that defines the API endpoints and their associated request details. This dictionary is structured in JSON format. Here's an example of how it should be organized:
-
-```json
-{
-    "end_point": "<url>",
-    "header":{
-        "<row>","value"
-    },
-    "<function_name>": {
-        "method": "<GET, POST, PUT, DELETE>",
-        "<json, parms>": {
-            "<var_name>": {},
-            ...
-        },
-        ...
-    },
-    ...
-}
-```
-
-To customize the dictionary for your specific API, replace `<url>` with the base URL of your API and `<function_name>`, `<method>`, `<json, parms>`, and `<var_name>` with appropriate values that correspond to your API's structure.
-Use `header` to set Auth Tokens
-### Example
-
-Here's a practical example to demonstrate how VRest can be utilized within your Python code:
+To interact with an API, you need to create a `RestAPI` instance by providing the API configuration in a dictionary format. The configuration should include the API endpoint, headers, and skeloton (available API functions).
 
 ```python
-# Import VRest
-from vrest import RestAPI
-# Import required modules
-import json
+api_config = {
+    "end_point": "https://api.example.com/v1/",
+    "header": {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "*Authorization": "Token {}"
+    },
+    "skeloton": {
+        "function_name": {
+            "suffix": "endpoint_suffix",
+            "method": "GET"
+        }
+        # Add more functions as needed
+    }
+}
 
-# Load the configuration dictionary from a file
-config = {}
-with open("<Dictionary_File>.json", "r") as config_file:
-    config = json.loads(config_file.read())
-
-# Create a RestAPI object
-api = RestAPI(config)
-
-# Set the API endpoint path
-api.path = "<Path>"
-
-# Set the command
-api.sub_dir = "<Command>"
-
-# Execute the API call
-api.exec({})
+# Create a RestAPI instance
+api = RestAPI(api_config, api_key="YOUR_API_KEY")
 ```
 
-Ensure that you replace `"<Dictionary_File>.json"` with the actual path to your configuration file. Customize `<Path>` and `<Command>` based on your specific API requirements.
+### Making API Requests
 
-## Contributing
+Once you have created a `RestAPI` instance, you can make API requests using the defined functions. For example, to make a GET request to the `function_name` endpoint:
 
-If you would like to contribute to the development of VRest, we welcome your contributions! Feel free to submit pull requests or open issues on the GitHub repository at [https://github.com/Vortex5Root/VRest](https://github.com/Vortex5Root/VRest).
+```python
+response = api.function_name(params={"param_name": "param_value"})
+```
 
-## License
+Replace `function_name` with the name of the function you want to call, and provide any necessary parameters as a dictionary in the `params` argument.
 
-VRest is released under the [MIT License](https://opensource.org/licenses/MIT), granting you the freedom to utilize and modify the library according to your needs.
+### WebSocket Support
+
+VRest also supports WebSocket connections. If a function is configured to use WebSocket (method="ws"), you can start a WebSocket connection and send and receive data. Here's an example of starting a WebSocket connection:
+
+```python
+async def start_websocket():
+    # Start the WebSocket connection
+    await api.function_name.ws_start(params={"param_name": "param_value"})
+
+# Create an asyncio event loop and run the WebSocket function
+import asyncio
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_websocket())
+```
+
+### Audio Streaming Example
+
+The provided code includes an audio streaming example using PyAudio and WebSocket. It captures audio from a microphone, sends it to the Deepgram API for speech recognition, and receives the transcribed text. You can adapt this example for your specific use case.
+
+## Error Handling
+
+VRest includes custom exceptions for handling common errors:
+
+- `InvalidEndPoint`: Raised when the API endpoint is invalid.
+- `MissingSkeloton`: Raised when the API configuration lacks a skeloton (function definitions).
+- `TokenRequired`: Raised when an API request requires an authentication token.
+
+You can handle these exceptions in your code to gracefully manage errors.
+
+## Conclusion
+
+VRest simplifies API interaction by providing a clean and flexible interface for making HTTP and WebSocket requests. You can use it to communicate with various APIs and easily handle responses and errors. Explore the library further and customize it to meet your specific needs.
--- vrest/__init__.py
+++ vrest/__init__.py
@@ -1,67 +1,178 @@
-from requests import request
-
-from typing import Dict,Any
-
 import json
+import asyncio
+import threading
+import websockets
 
-class RestAPI(object):
+from requests import Session
+from websockets.sync.client import connect
 
-    dir_       : str = ""
-    function_  : str
-    dictionary : Dict
+from typing import Dict,Any, List
 
-    def __init__(self,paper : Dict) -> None:
-        self.dictionary = paper
+class InvalidEndPoint(Exception):
+    def __init__(self,endpoint : str) -> None:
+        self.endpoint = endpoint
+        self.message = "Invalid EndPoint: {}".format(self.endpoint)
+        super().__init__(self.message)
 
-    def check_dir(self, dir_ : str):
-        return dir_ in [_ for _ in self.dictionary]
+class MissingSkeloton(Exception):
+    def __init__(self, *args: object) -> None:
+        self.message = "Missing Skeloton"
+        super().__init__(self.message)
 
-    def check_function(self,function_ : str):
-        return function_ in [_ for _ in self.dictionary[self.dir_]]
+class TokenRequired(Exception):
+    def __init__(self, *args: object) -> None:
+        self.message = "Token Required"
+        super().__init__(self.message)
+
+queue_input = asyncio.Queue()
+queue_output = asyncio.Queue()
+class Function:
+    end_point : str
+    method    : str
+    session   : Session
+
+    ws        : bool = False
+    ws_output : List[Any] = []
+
+    def __init__(self,session : Session,config : Dict) -> None:
+        # Set the session
+        self.session = session
+        # Set the Function Config
+        self.end_point = config["end_point"]
+        self.method = config["method"]
+        # Prepare the Function
+        self.prepare()
     
-    def check_agrs(self, check : Any,args : Dict):
-        if check != {}:
-            return len([_ for _ in args if _ not in [__ for __ in check]]) == 0
-        return True
-    
-    @property
-    def sub_dir(self) -> str:
-        return self.fuction_
-    
-    @sub_dir.setter
-    def sub_dir(self,value : str) -> None:
-        if self.check_function(value):
-            self.function_ = value
 
-    @property
-    def path(self) -> str:
-        return self.dir_
-    
-    @path.setter
-    def path(self,value : str) -> None:
-        if self.check_dir(value):
-            self.dir_ = value
+    def prepare(self):
+        # Check if WebSocket is enabled
+        if self.method.lower() == "ws":
+            # Set WebSocket to True
+            self.ws = True
+            # Check if the end_point is valid
+            if self.end_point.startswith("https://"):
+                # Convert https with wss
+                self.end_point = self.end_point.replace("https","wss")
+            elif self.end_point.startswith("http://"):
+                # Convert http with ws
+                self.end_point = self.end_point.replace("http","ws")
+            elif not self.end_point.startswith("ws://") and not self.end_point.startswith("wss://"):
+                # Raise an error if the end_point is not valid
+                raise "Invalid EndPoint: {}".format(self.end_point)
+            # Set the header
+            self.header = {"Authorization" : self.session.headers["Authorization"]}
+        # Check if the method is valid
+        elif self.method.lower() not in ["get","post","put","delete","ws"]:
+            raise "Invalid Method: {}".format(self.method)
 
-    def execute(self,args : Dict) -> Dict:
-        if self.check_dir(self.dir_):
-            if self.check_function(self.function_):
-                data = self.dictionary[self.dir_][self.function_]
-                if "json" in [_ for _ in data]:
-                    if not self.check_agrs(data["json"],args):
-                        raise "Invalid args\n Rules: {}".format(data["json"])
-                    data["json"] = args
-                if "params" in [_ for _ in data]:
-                    if not self.check_agrs(data["params"],args):
-                        raise "Invalid args\n Rules: {}".format(data["json"])
-                    data["params"] = args
-                data["url"] = self.dictionary["end_point"]+self.dir_
-                data["headers"] = self.dictionary["header"]
-                output = request(**data)
-                try:
-                    return output.json()
-                except:
-                    return output
-            else:
-                raise "Invalid Function"
+    # This function is used to set the params of the websocket connection
+    def set_params_ws(self,params : Dict = {}):
+        # Check if WebSocket is enabled
+        if self.ws:
+            # Check if the end_point has params
+            if params != {}:
+                # Set URI params
+                self.end_point += "?"
+                self.end_point += "&".join([f"{key}={params[key]}" for key in params.keys()])
+    # Start the WebSocket Client
+    async def ws_start(self,params : Dict = {}):
+        # Check if WebSocket is enabled
+        if self.ws:
+            # Set URI params
+            self.set_params_ws(params)
+            try:
+                # WebSocket connection Variables
+                uri = self.end_point
+                headers = self.header
+                # Connect to the WebSocket
+                async with websockets.connect(uri, extra_headers=headers) as ws:
+                    # Define the receiver and sender functions
+                    async def receiver(ws):
+                        data = []
+                        # Receive data from the WebSocket
+                        async for msg in ws:
+                            await queue_output.put(json.loads(msg))
+                    # Define the sender function
+                    async def sender(ws):
+                        try:
+                            # Send data to the WebSocket
+                            while True:
+                                # Get data from the queue
+                                data = await queue_input.get()
+                                # Send data to the WebSocket
+                                await ws.send(data)                        
+                        except websockets.exceptions.ConnectionClosedOK:
+                            await ws.send(json.dumps({"type": "CloseStream"}))
+                        except Exception as e:
+                            print(e)
+                            pass
+                    # Start the receiver and sender functions
+                    functions = [
+                        asyncio.ensure_future(sender(ws)),
+                        asyncio.ensure_future(receiver(ws)),
+                    ]
+                    # Wait for the functions to finish
+                    await asyncio.gather(*functions)          
+            except websockets.exceptions.ConnectionClosedError as e:
+                print(f"WebSocket connection closed unexpectedly: {e}")
         else:
-            raise "Invalid Dir"
+            raise Exception("Not a WebSocket\n Please use start(params) to start the connection")
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        if not self.ws:
+            return self.session.request(self.method,self.end_point,**kwds)
+        else:
+            if args != {}:
+                queue_input.put_nowait(args)
+                output = queue_output.get_nowait()
+                if output is not None:
+                    return output
+                return None
+            
+            
+class RestAPI:
+
+    paper : Dict
+    # session Data
+    url     : str
+    headers : Dict
+    session : Session
+    skeloton: Dict
+
+    def __init__(self,paper : Dict,token=None) -> None:
+        self.paper = paper
+        self.key = token
+        self.start()
+
+    def start(self):
+        if "end_point" not in self.paper.keys():
+            raise InvalidEndPoint("")
+        if "skeloton" not in self.paper.keys():
+            raise MissingSkeloton()
+        if "header" not in self.paper.keys():
+            self.paper["header"] = {}
+        self.url = self.paper["end_point"]
+        self.headers = self.paper["header"]
+        for key in self.headers:
+            if key.startswith("*"):
+                new_key = key.replace("*","")
+                if self.key is None:
+                    raise TokenRequired("")
+                self.headers[new_key] = self.headers[key].format(self.key)
+                del self.headers[key]
+                break
+        self.session = Session()
+        self.session.headers.update(self.headers)
+        self.skeloton = self.paper["skeloton"]
+        self.load_functions()
+
+    def load_functions(self):
+        for key in self.skeloton:
+            if self.url.endswith("/"):
+                self.url = self.url[:-1]
+            end_point = self.skeloton[key]["suffix"]
+            _ = {
+                "end_point" : f"{self.url}/{self.skeloton[key]['suffix']}",
+                "method"    : self.skeloton[key]["method"],
+            }
+            setattr(self,key,Function(self.session,_))
